b8b1e73cb7b7583982f73fb4f860c0f1767e33e2
rick <technoweenie@gmail.com>
2011-12-30 23:35:13 -0700

    add a basic serializer

--- lib/horcrux.rb
+++ lib/horcrux.rb
@@ -3,11 +3,12 @@ module Horcrux
 
   module Methods
     def self.included(klass)
-      klass.send :attr_reader, :client
+      klass.send :attr_reader, :client, :serializer
     end
 
-    def initialize(client)
+    def initialize(client, serializer = nil)
       @client = client
+      @serializer = serializer || StringSerializer.new
     end
 
     def get_all(*keys)
@@ -31,25 +32,39 @@ module Horcrux
     def fetch(key)
       get(key) || set(key, yield)
     end
+
+    def key_for(key)
+      key.to_s
+    end
+  end
+
+  class StringSerializer
+    def pack(value)
+      value.to_s
+    end
+
+    def unpack(str)
+      str
+    end
   end
 
   class Memory
     include Methods
 
     def key?(key)
-      client.key? key
+      client.key? key_for(key)
     end
 
     def get(key)
-      client[key]
+      serializer.unpack client[key_for(key)]
     end
 
     def set(key, value)
-      client[key] = value
+      client[key_for(key)] = serializer.pack(value)
     end
 
     def delete(key)
-      !client.delete(key).nil?
+      !client.delete(key_for(key)).nil?
     end
   end
 end
