668fe54b5bd8073255cdfe61d8f20c756956a5be
rick <technoweenie@gmail.com>
2011-12-30 23:16:17 -0700

    worry about this crap when we need it

--- lib/horcrux.rb
+++ lib/horcrux.rb
@@ -17,18 +17,6 @@ module Horcrux
     def fetch(key)
       get(key) || set(key, yield)
     end
-
-    def key_for(key)
-      key.to_s
-    end
-
-    def encode(value)
-      value.to_s
-    end
-
-    def decode(value)
-      value
-    end
   end
 
   class Memory
