9ab777595bcbe14c649f2e5677c76364323fdf97
rick <technoweenie@gmail.com>
2012-01-09 14:51:15 -0700

    tomdoc the multiple adapter

--- lib/horcrux/multiple.rb
+++ lib/horcrux/multiple.rb
@@ -1,10 +1,29 @@
 module Horcrux
   class Multiple
     include Methods
+
+    # Sets up an Adapter using a collection of other adapters.  The first is
+    # assumed to be the main, while the others are write-through caches.  This
+    # is good for caching.
+    #
+    #     mysql = Horcrux::MysqlAdapter.new ... # fake
+    #     memcache = Horcrux::MemcacheAdapter.new ... # fake
+    #     adapter = Horcrux::Multiple.new mysql, memcache
+    #
+    # Reads will hit the secondary adapters before the main.  Writes will hit
+    # the main adapter first, before being sent to the secondary adapters.
+    #
+    # *adapters - One or more Horcrux-compliant adapters.
     def initialize(*adapters)
+      if adapters.empty?
+        raise ArgumentError, "Need at least 1 adapter."
+      end
+
       @main = adapters.shift
-      @readers = adapters
+      @adapters = adapters
     end
+
+    ## HORCRUX METHODS
 
     def key?(key)
       read_cache :key?, key
@@ -30,16 +49,48 @@ module Horcrux
       write_through :delete_all, *keys
     end
 
+    ## INTERNAL
+
+    # Writes the data to the main adapter first, and then to the other
+    # adapters.
+    #
+    # method - A Symbol identifying the method to call.
+    # *args  - One or more arguments to send to the methods.
+    #
+    # Returns the result of the method call on the main adapter.
     def write_through(method, *args)
       result = @main.send(method, *args)
-      @readers.each { |r| r.send(method, *args) }
+      @adapters.each do |adapter|
+        call_adapter adapter, method, *args
+      end
       result
     end
 
+    # Reads the data from the other adapters before the main adapter.
+    #
+    # method - A Symbol identifying the method to call.
+    # *args  - One or more arguments to send to the methods.
+    #
+    # Returns the result of the first adapter to respond with a value.
     def read_cache(method, *args)
       value = nil
-      @readers.detect { |r| value = r.send(method, *args) }
+      @adapters.detect do |adapter| 
+        value = call_adapter adapter, method, *args
+      end
       value || @main.send(method, *args)
+    end
+
+    # Calls the given adapter, swallowing up any error.
+    #
+    # adapter - A Horcrux adapter.
+    # method - A Symbol identifying the method to call.
+    # *args  - One or more arguments to send to the methods.
+    #
+    # Returns the value of the method call.
+    def call_adapter(adapter, method, *args)
+      adapter.send(method, *args)
+    rescue err
+      $stderr.puts "Exception for #{adapter.inspect}##{method}: #{err}"
     end
   end
 end
--- test/multiple_test.rb
+++ test/multiple_test.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../helper', __FILE__)
+require File.expand_path('../memory_test', __FILE__)
 
 module Horcrux
   class MultipleTest < MemoryTest
